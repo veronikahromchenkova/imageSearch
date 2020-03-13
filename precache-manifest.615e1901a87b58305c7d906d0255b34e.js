105753bfbe605295d75be4c365f37c9285a213a2
Veronika Hromchenkova <veronika.hromchenkova@mediapark.com>
2020-03-13 10:30:02 +0200

    Updates

---
+++ precache-manifest.615e1901a87b58305c7d906d0255b34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86613c8e1321ff10f5d9f59084267f8a",
+    "url": "/imageSearch/index.html"
+  },
+  {
+    "revision": "8e37e209680e870a3f8f",
+    "url": "/imageSearch/static/css/main.5b8a37ac.chunk.css"
+  },
+  {
+    "revision": "749f2cc2cdd4d146c560",
+    "url": "/imageSearch/static/js/2.aa9ae96d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/imageSearch/static/js/2.aa9ae96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e37e209680e870a3f8f",
+    "url": "/imageSearch/static/js/main.4d9c321f.chunk.js"
+  },
+  {
+    "revision": "17c9f0c77661ba11749e",
+    "url": "/imageSearch/static/js/runtime-main.d921b2b9.js"
+  }
+]);
