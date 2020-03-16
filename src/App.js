ec68a0bc4a6221c7a2ef53cecd8c925c06cc2189
Veronika Hromchenkova <veronika.hromchenkova@mediapark.com>
2020-03-16 16:26:19 +0200

    separate functions and components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './App.scss';
-import axios from 'axios';
+import { getImages } from './components/functions'
 import ImageList from './components/imageList';
 import SearchBar from "./components/searchBar";
 import Loader from 'react-loader-spinner'
 import InfiniteScroll from 'react-infinite-scroll-component';
-require('dotenv').config();
 
 function App() {
 
-    const clientId = process.env.REACT_APP_CLIENT_ID;
     const [keyword, setKeyword] = useState("");
     const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -19,44 +17,30 @@ function App() {
     const [fetchLimit, setFetchLimit] = useState(0);
     const [endMessage, setEndMessage] = useState('You have seen it all');
     const [noResultText, setNoResultText] = useState('');
+    const maxFetchLimit = 2;
 
 
     useEffect(() => {
         document.title = title;
         setFetchLimit(0);
-
     },[title]);
 
-    async function searchImages(page= 1, per_page= 30){
+    function searchImages(page=1, per_page=30) {
         setLoading(true);
         setFetchLimit(fetchLimit + 1);
-        if(fetchLimit < 2) {
-            const url = "https://api.unsplash.com/search/photos";
-            await axios.get(url, {
-                params: {
-                    query: keyword,
-                    page: page,
-                    per_page: per_page,
-                },
-                headers: {
-                    Authorization: 'Client-ID ' + clientId
-                }
-            })
-                .then(response => {
+        if(fetchLimit < maxFetchLimit){
+            getImages(page, per_page, keyword)
+                .then((response) => {
                     setLoading(false);
                     setTitle(keyword + " Pictures");
-                    const newImages = [...images, ...response.data.results];
+                    const newImages = [...images, ...response.results];
                     setImages(newImages);
-                    if(response.data.total === 0){
+                    if(response.total === 0){
                         setNoResultText('No Results');
                     } else{
                         setNoResultText('');
                     }
                 })
-                .catch(err => {
-                    setHasMore(false);
-                    console.log(err);
-                });
         } else{
             setHasMore(false);
             setEndMessage('You have reached limit');
